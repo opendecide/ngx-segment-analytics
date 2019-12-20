39b5bd5c788ccb9e2d4c059ed46df5e3bf4b172a
Guillaume RODRIGUEZ <g.rodriguez@opendecide.com>
2019-12-20 11:43:00 +0100

    Limit exportation scope of module

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export * from './ngx-segment-analytics.module';
-export { SegmentService } from './ngx-segment-analytics.service';
-export { SegmentConfig } from './ngx-segment-analytics.config';
+export {SEGMENT_CONFIG, SegmentModule} from './ngx-segment-analytics.module';
+export {SegmentService} from './ngx-segment-analytics.service';
+export {SegmentConfig} from './ngx-segment-analytics.config';
