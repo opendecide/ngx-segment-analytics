c5ef44e14ef33c4df42ac296356c01a65d0834f6
Guillaume RODRIGUEZ <g.rodriguez@opendecide.com>
2020-01-02 11:34:42 +0100

    Revert "Limit exportation scope of module"
    This reverts commit 39b5bd5c788ccb9e2d4c059ed46df5e3bf4b172a.
    
    Fix #97

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export {SEGMENT_CONFIG, SegmentModule} from './ngx-segment-analytics.module';
-export {SegmentService} from './ngx-segment-analytics.service';
-export {SegmentConfig} from './ngx-segment-analytics.config';
+export * from './ngx-segment-analytics.module';
+export { SegmentService } from './ngx-segment-analytics.service';
+export { SegmentConfig } from './ngx-segment-analytics.config';
